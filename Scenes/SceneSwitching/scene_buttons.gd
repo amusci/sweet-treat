5fb3ba801b08c125b0dbed47cb0dc721319103db
a <71357098+amusci@users.noreply.github.com>
2025-08-05 18:50:30 -0400

    Scene Buttons animations
    Made transition screen a singleton
    Scene Buttons now show animations

--- Scenes/SceneSwitching/scene_buttons.gd
+++ Scenes/SceneSwitching/scene_buttons.gd
@@ -1,16 +1,35 @@
 extends HBoxContainer
 
-func _on_baking_button_pressed():
-	InventoryManager.save_all_inventory_slots()
-	get_tree().change_scene_to_file("res://Scenes/Areas/bakery_playground.tscn")
-
-func _on_storage_button_pressed():
-	InventoryManager.save_all_inventory_slots()
-	get_tree().change_scene_to_file("res://Scenes/Areas/storage_playground.tscn")
 
 func _ready():
-	# Wait for all nodes to be ready, then restore inventory
 	call_deferred("restore_inventory")
 
 func restore_inventory():
+	# For the call_deferred
 	InventoryManager.restore_all_inventory_slots()
+
+func _on_baking_button_pressed():
+	# Baking Button Functionality
+	if _get_current_scene("res://Scenes/Areas/bakery_playground.tscn"):
+		return
+	
+	InventoryManager.save_all_inventory_slots()
+	TransitionScreen.transition()
+	
+	await get_tree().create_timer(0.5).timeout
+	get_tree().change_scene_to_file("res://Scenes/Areas/bakery_playground.tscn")
+
+func _on_storage_button_pressed():
+	# Storage Button Functionality
+	if _get_current_scene("res://Scenes/Areas/storage_playground.tscn"):
+		return
+	InventoryManager.save_all_inventory_slots()
+	TransitionScreen.transition()
+	
+	await get_tree().create_timer(0.5).timeout
+	get_tree().change_scene_to_file("res://Scenes/Areas/storage_playground.tscn")
+	
+func _get_current_scene(scene_path: String) -> bool:
+	# Helper function to return if we are trying to access the same scene we are in
+	var current_scene = get_tree().current_scene.scene_file_path
+	return current_scene == scene_path
