87aee7a2f94804e97cdc6bfe011279c0a21560fa
a <71357098+amusci@users.noreply.github.com>
2025-08-10 21:49:37 -0400

    Show Recipe on the order tooltip
    OrderManager now removes order by data instead of == the name
    Order now shows the recipe user needs to make on tooltip
    
    i need ui help so bad please

--- Scenes/Orders/order.gd
+++ Scenes/Orders/order.gd
@@ -4,24 +4,69 @@ extends VBoxContainer
 ######## Needs to show sprite + SPRITE + sPrItE = RECIPE
 ######## Need to also show previous inbetween recipes.. no clue how to do this...
 
-
 var progress_bar: ProgressBar
 var label: Label
 var recipe
+var recipe_container: HBoxContainer
 var time_left: float
 var max_time: float
+var order_data
 
-func setup(order_data):
+func setup(order_data_param):
 	# Order setup functionality
+	order_data = order_data_param # New variable alert
 	# Need to load these two as they are instantiated
 	progress_bar = get_node("ProgressBar") 
 	label = get_node("Label")
-	# Then set the UI to the data
+
+	recipe_container = get_node("RecipeContainer")
+	
+	# Clear any existing recipe display
+	for child in recipe_container.get_children():
+		child.queue_free()
+	
+	# Add ingredient sprites
+	for i in range(order_data.ingredients.size()):
+		var recipe_requirement = order_data.ingredients[i]
+		var actual_ingredient = recipe_requirement.ingredient
+		
+		# Create sprite for ingredient
+		var ingredient_sprite = TextureRect.new()
+		ingredient_sprite.texture = actual_ingredient.icon
+		ingredient_sprite.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_CENTERED
+		ingredient_sprite.custom_minimum_size = Vector2(32, 32) # We are doing 32x32 right?
+		recipe_container.add_child(ingredient_sprite)
+		
+		# Add + between ingredients
+		if i < order_data.ingredients.size() - 1:
+			var plus_label = Label.new()
+			plus_label.text = " + "
+			plus_label.add_theme_font_size_override("font_size", 16)
+			plus_label.add_theme_color_override("font_color", Color.BLACK)
+			recipe_container.add_child(plus_label)
+	
+	# Add equals sign to signify output
+	var equals_label = Label.new()
+	equals_label.text = " = "
+	equals_label.add_theme_font_size_override("font_size", 16)
+	equals_label.add_theme_color_override("font_color", Color.BLACK)
+	recipe_container.add_child(equals_label)
+	
+	# Add final recipe 
+	var recipe_sprite = TextureRect.new()
+	recipe_sprite.texture = order_data.recipe.icon
+	recipe_sprite.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_CENTERED
+	recipe_sprite.custom_minimum_size = Vector2(32, 32) # 32x32
+	recipe_container.add_child(recipe_sprite)
+	
+	# Set up the rest of the order data
 	recipe = order_data.recipe
 	time_left = order_data.time_left
-	max_time = recipe.time_to_make
+	max_time = order_data.recipe.time_to_make
+	# Show recipe name
+	label.text = "Order: %s" % order_data.recipe.title
+
 	
-	label.text = recipe.title
 	progress_bar.max_value = 100.0
 	progress_bar.value = 100.0
 
@@ -34,4 +79,5 @@ func _process(delta):
 		
 		if time_left <= 0:
 			# Order expired
-			OrderManager.remove_order(self)
+			OrderManager.remove_order_by_data(order_data)
+			queue_free()
--- Scripts/Singleton/OrderManager.gd
+++ Scripts/Singleton/OrderManager.gd
@@ -6,7 +6,7 @@ extends Node
 var available_recipes: Array = []
 var active_orders: Array = []
 @export var max_orders := 3
-@export var spawn_interval := 5.0
+@export var spawn_interval := 1.0
 var spawn_timer := 0.0
 
 signal order_added(order_data)
@@ -14,14 +14,14 @@ signal order_removed(order_node)
 
 func _ready():
 	# Make sure DataManager already loaded the recipes
-	if DataManager.finished_recipes.size() > 0:
+	if DataManager.inbetween_recipes.size() > 0:
 		init_recipes()
 	else:
 		print("No finished recipes available!")
 
 func init_recipes():
 	# Put the available recipes into an array
-	available_recipes = DataManager.finished_recipes.values()
+	available_recipes = DataManager.inbetween_recipes.values()
 	print("Loaded ", available_recipes.size(), " recipes:")
 	for recipe in available_recipes:
 		print("- ", recipe.title)
@@ -38,19 +38,19 @@ func spawn_order():
 	var recipe = available_recipes.pick_random() # Choose random recipe
 	var order_data = { # House recipe data in dictionary
 		"recipe": recipe,
-		"time_left": recipe.time_to_make
+		"time_left": recipe.time_to_make,
+		"ingredients" : recipe.ingredients,
 	}
-	
 	active_orders.append(order_data) # Create new order in array
 	print("New order: ", recipe.title, " (", active_orders.size(), "/", max_orders, ")")
 	order_added.emit(order_data) # Tell our container to add an order
 
-func remove_order(order_node):
-	# Remove order functionality
-	for i in range(active_orders.size()):
-		if active_orders[i].recipe == order_node.recipe: # Make sure our array aligns with the UI
-			active_orders.remove_at(i) # Remove that order from the array
-			break
-	
-	print("Order removed: ", order_node.recipe.title, " (", active_orders.size(), "/", max_orders, ")")
-	order_removed.emit(order_node) # Tell our container to remove the order
+func remove_order_by_data(order_data):
+	var index = active_orders.find(order_data)
+	if index != -1:
+		active_orders.remove_at(index)
+		print("Removed order: ", order_data.recipe.title, " (", active_orders.size(), "/", max_orders, ")")
+		# Note: You'll need to handle the UI removal in your container script
+		# since we don't have the order_node here to emit with
+	else:
+		print("Error: Could not find order to remove!")
