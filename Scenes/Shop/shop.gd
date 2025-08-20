3569ce509e92cb6a81639b10f1ef0e1f6833cc73
a <71357098+amusci@users.noreply.github.com>
2025-08-20 16:48:29 -0400

    Shop setup
    Im not sure if I need the button onreadys but ima capture them anyways

---
+++ Scenes/Shop/shop.gd
@@ -0,0 +1,22 @@
+extends Control
+
+@onready var baking_soda_button = $ScrollContainer/VBoxContainer/BakingSodaSlot/BakingSodaButton
+@onready var baking_soda_slot = $ScrollContainer/VBoxContainer/BakingSodaSlot/BakingSodaSlot
+
+@onready var egg_slot = $ScrollContainer/VBoxContainer/EggSlot/EggSlot
+@onready var egg_button = $ScrollContainer/VBoxContainer/EggSlot/EggButton
+
+@onready var flour_slot = $ScrollContainer/VBoxContainer/Flour/FlourSlot
+@onready var flour_button = $ScrollContainer/VBoxContainer/Flour/FlourButton
+
+@onready var sea_salt_slot = $ScrollContainer/VBoxContainer/SeaSalt/SeaSaltSlot
+@onready var sea_salt_button = $ScrollContainer/VBoxContainer/SeaSalt/SeaSaltButton
+
+@onready var sugar_slot = $ScrollContainer/VBoxContainer/Sugar/SugarSlot
+@onready var sugar_button = $ScrollContainer/VBoxContainer/Sugar/SugarButton
+
+@onready var vanilla_extract_slot = $ScrollContainer/VBoxContainer/VanillaExtract/VanillaExtractSlot
+@onready var vanilla_extract_button = $ScrollContainer/VBoxContainer/VanillaExtract/VanillaExtractButton
+
+@onready var butter_slot = $ScrollContainer/VBoxContainer/Butter/ButterSlot
+@onready var butter_button = $ScrollContainer/VBoxContainer/Butter/ButterButton
