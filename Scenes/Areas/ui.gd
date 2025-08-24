85f4cfd7cb03561ee77d238966e6c859d9bece11
a <71357098+amusci@users.noreply.github.com>
2025-08-23 22:42:48 -0400

    SFX Manager and pause menu SFX
    Able to save SFX volume
    Able to play any preloaded SFX

--- Scenes/Areas/ui.gd
+++ Scenes/Areas/ui.gd
@@ -3,6 +3,9 @@ extends Node2D
 @onready var pause_menu = $PauseMenu
 @onready var order_container = $OrderContainer
 
+var open_menu = preload("res://Assets/SFX/openmenu.wav")
+var close_menu = preload("res://Assets/SFX/closemenu.wav")
+
 var not_showing := false
 
 func _ready():
@@ -15,3 +18,9 @@ func _process(_delta):
 		pause_menu.visible = not_showing
 		order_container.visible = !not_showing
 		Engine.time_scale = 1.0 if !not_showing else 0.0
+	
+	if not_showing == true and Input.is_action_just_pressed("pause_menu"):
+		SfxManager.play_SFX(open_menu)
+	if not_showing == false and Input.is_action_just_pressed("pause_menu"):
+		SfxManager.play_SFX(close_menu)
+	
