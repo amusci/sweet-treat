e44268cfac9cd1df7ddb3d9b7da34cd3123659e4
a <71357098+amusci@users.noreply.github.com>
2025-08-25 16:01:44 -0400

    pixel art font

--- Scripts/ResourceClasses/MoneyUI.gd
+++ Scripts/ResourceClasses/MoneyUI.gd
@@ -12,7 +12,6 @@ func _process(_delta):
 		MachineManager.clear_all_machine_data()
 		InventoryManager.clear_all_inventory_slots()
 		get_tree().change_scene_to_file(game_over)
-		
 	
 func _on_money_changed(new_amount): #has _ before because it is connected to a signal
 	money_label.text = str(new_amount) #changes the text in the label to new amount value in the Money script
