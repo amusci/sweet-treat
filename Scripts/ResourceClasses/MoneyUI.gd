b739352eb782ebd867323654fcd5055abcb12232
a <71357098+amusci@users.noreply.github.com>
2025-09-01 17:15:48 -0400

    i dont know why i changed the ready function without changing the actual money changing function

--- Scripts/ResourceClasses/MoneyUI.gd
+++ Scripts/ResourceClasses/MoneyUI.gd
@@ -14,4 +14,4 @@ func _process(_delta):
 		get_tree().change_scene_to_file(game_over)
 	
 func _on_money_changed(new_amount): #has _ before because it is connected to a signal
-	money_label.text = str(new_amount) #changes the text in the label to new amount value in the Money script
+	money_label.text = "$" + str(new_amount) #changes the text in the label to new amount value in the Money script
