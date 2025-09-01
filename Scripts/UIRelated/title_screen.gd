469ed68a0daf3447b08ec3bcba27fd756112c361
a <71357098+amusci@users.noreply.github.com>
2025-09-01 19:02:43 -0400

    move settings to title screen
    i am a boolean wizard

--- Scripts/UIRelated/title_screen.gd
+++ Scripts/UIRelated/title_screen.gd
@@ -4,9 +4,22 @@ extends Control
 @onready var settings_button = $Settings
 @onready var exit_button = $Exit
 
+@onready var settings_toggle = $SettingsToggle
+@onready var sfx_slider = $SettingsToggle/SFXSlider
+@onready var button = $SettingsToggle/Button
+@onready var music_slider = $SettingsToggle/MusicSlider
+
 var starting_money := 1000
+var show_settings := false
 
 func _ready():
+	show_settings = false
+	settings_toggle.visible = false
+	
+	# Set slider to saved volume
+	music_slider.value = MusicManager.get_current_volume()
+	sfx_slider.value = SfxManager.get_current_volume()
+	
 	OrderManager.orders_spawning = false
 	Money.money = starting_money # This will reset the players money 
 	var music = preload("res://Assets/Music/Test Song.wav")
@@ -23,9 +36,24 @@ func _on_play_pressed() -> void:
 
 func _on_settings_pressed() -> void:
 	SfxManager.play_SFX(SfxManager.click)
-	get_tree().change_scene_to_file("res://Scenes/Areas/UIScenes/Settings.tscn")
+	show_settings = !show_settings
+	settings_toggle.visible = show_settings
+	#get_tree().change_scene_to_file("res://Scenes/Areas/UIScenes/Settings.tscn")
 	print("Settings Pressed")
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
 	print("Exit Pressed")
+	
+func _on_music_slider_value_changed(value):
+	MusicManager.set_volume(value)
+	print("Volume Changed ", value)
+
+
+func _on_sfx_slider_value_changed(value):
+	SfxManager.set_volume(value)
+	print("Volume Changed ", value)
+
+
+func _on_button_pressed():
+	SfxManager.play_SFX(SfxManager.no)
